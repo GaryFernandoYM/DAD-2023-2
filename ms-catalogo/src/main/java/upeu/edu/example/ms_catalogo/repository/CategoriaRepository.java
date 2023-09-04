1e6c5ca3380cf4bf521620c748830d8d1569e648
GaryFernandoYM <miraclevann224@gmail.com>
2023-09-04 10:51:48 -0500

    cliente

--- ms-catalogo/src/main/java/upeu/edu/example/ms_catalogo/repository/CategoriaRepository.java
+++ ms-catalogo/src/main/java/upeu/edu/example/ms_catalogo/repository/CategoriaRepository.java
@@ -3,5 +3,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import upeu.edu.example.ms_catalogo.entity.Categoria;
 
 public interface CategoriaRepository extends
-        JpaRepository<Categoria,Integer> {
+        JpaRepository<Categoria,Integer > {
 }
